7d61043b400a6b8ea354b5fe34dce9ea15282d71
Bongumusa Nala <bongimusa.nala@gmail.com>
2022-11-19 14:45:00 +0200

    2

--- Derivco/Models/Ingredients.cs
+++ Derivco/Models/Ingredients.cs
@@ -8,16 +8,16 @@ namespace Derivco.Models
 {
     public class Ingredients
     {
-        public string id { get; set; }
-        public string i { get; set; }
+        public string idIngredient { get; set; }
+        public string strIngredient { get; set; }
 
-        public string Description { get; set; }
+        public string strDescription { get; set; }
 
-        public string Type { get; set; }
+        public string strType { get; set; }
 
-        public string Alcohol { get; set; }
+        public string strAlcohol { get; set; }
 
-        public string ABV { get; set; }
+        public string strABV { get; set; }
 
     }
 }
--- Derivco/UnitTest1.cs
+++ Derivco/UnitTest1.cs
@@ -4,7 +4,9 @@ using Newtonsoft.Json.Linq;
 using RestSharp;
 using System.Net.Security;
 using Xunit.Abstractions;
-using NUnit.Framework;
+
+using System;
+using System.ComponentModel;
 
 namespace Derivco
 {
@@ -62,10 +64,11 @@ namespace Derivco
         }
 
 
-    
 
+        // The below Test Case Searching for a cocktail by name is case-insensitive
         [Fact]
-        public async Task GetNonAlcohol()
+         public async Task SeacrhCockTailNameIncase()
+       
         {
 
             var restClientOptions = new RestClientOptions
@@ -75,19 +78,23 @@ namespace Derivco
             };
             // Rest Client initialization
             var client = new RestClient(restClientOptions);
-            //Rest Request
+            //Rest Request and the prameters used
 
             var request = new RestRequest("api/json/v1/1/search.php/");
-            request.AddQueryParameter("i", "vodka");
+            request.AddQueryParameter("s", "margarita");
+
+            //Below is the Request response
 
             var response = await client.GetAsync<Ingredients>(request);
-            response?.Alcohol.Should().BeNull();
-            response?.ABV.Should().NotBeNull();
+
+            // Below is the assertion for the Name to be in case Sensitive
+            response?.Alcohol.Should().NotBeUpperCased("margarita");
+           
 
         }
 
         [Fact]
-         public async Task GetAlcohol()
+         public async Task SeacrhameIncase()
         {
             var restClientOptions = new RestClientOptions
             {
@@ -99,7 +106,7 @@ namespace Derivco
             //Rest Request
 
             var request = new RestRequest("api/json/v1/1/search.php/");
-            request.AddQueryParameter("i", "vodka");
+            request.AddQueryParameter("s", "margarita");
 
             var response = await client.GetAsync<Ingredients>(request);
             response?.Alcohol.Should().BeNull();
@@ -121,8 +128,9 @@ namespace Derivco
             var request = new RestRequest("api/json/v1/1/search.php/");
             request.AddQueryParameter("s", "margarita");
 
-            var response = await client.GetAsync<Cocktails>(request);
-            response?.strDrink.Should().BeUpperCased();
+            var response = await client.GetAsync(request);
+            //await response.Should().("AB");
+            response.Should().BeSameAs("Alcoholic");
             
 
         }
@@ -141,12 +149,37 @@ namespace Derivco
             //Rest Request
 
             var request = new RestRequest("api/json/v1/1/search.php/");
+
+            // Adding the parameter request for the cocktail that does not exists
+            request.AddQueryParameter("s", "margaritas");
+
+            // The response for cocktail that does not exist
+            var response = await client.GetAsync<Cocktails>(request);
+            
+            response?.strDrink.Should().BeNull();
+            
+        }
+
+        [Fact]
+        public async Task SearchCocktailName()
+        {
+
+            var restClientOptions = new RestClientOptions
+            {
+                BaseUrl = new Uri("https://www.thecocktaildb.com/"),
+                RemoteCertificateValidationCallback = (Sender, certificate, chain, errors) => true
+            };
+            // Rest Client initialization
+            var client = new RestClient(restClientOptions);
+            //Rest Request
+
+            var request = new RestRequest("api/json/v1/1/search.php/");
             request.AddQueryParameter("s", "margarita");
-            request.AddQueryParameter("s", " ");
+           
 
             var response = await client.GetAsync<Cocktails>(request);
-            response?.drinks.Should().BeNull();
-            
+            response?.strDrink.Should().ContainAny("m");
+
         }
 
     }
